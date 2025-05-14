d219250bc81a3328414ddb76b4de354fcc8f1f60
lqzs <lqzs@proton.me>
2025-05-14 19:03:24 +0600

    Add connection status configuration for awesome_oled and update output widget handling

--- boards/shields/awesome_oled/widgets/output.c
+++ boards/shields/awesome_oled/widgets/output.c
@@ -7,7 +7,7 @@ LV_IMG_DECLARE(bt_unbonded);
 LV_IMG_DECLARE(bt);
 LV_IMG_DECLARE(usb);
 
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL) || IS_ENABLED(CONFIG_NICE_OLED_CONN_STATUS)
 static void draw_usb_connected(lv_obj_t *canvas) {
   lv_draw_img_dsc_t img_dsc;
   lv_draw_img_dsc_init(&img_dsc);
@@ -33,7 +33,7 @@ static void draw_ble_disconnected(lv_obj_t *canvas) {
   lv_canvas_draw_img(canvas, 4, 32, &bt_no_signal, &img_dsc);
   // lv_canvas_draw_img(canvas, 49, 0, &bt_no_signal, &img_dsc);
 }
-
+#if IS_ENABLED(CONFIG_NICE_OLED_CONN_STATUS)
 static void draw_ble_connected(lv_obj_t *canvas) {
   lv_draw_img_dsc_t img_dsc;
   lv_draw_img_dsc_init(&img_dsc);
@@ -41,7 +41,7 @@ static void draw_ble_connected(lv_obj_t *canvas) {
   lv_canvas_draw_img(canvas, 4, 32, &bt, &img_dsc);
   // lv_canvas_draw_img(canvas, 49, 0, &bt, &img_dsc);
 }
-
+#endif
 void draw_output_status(lv_obj_t *canvas, const struct status_state *state) {
   /*
    * WHITOUT BACKGROUND
